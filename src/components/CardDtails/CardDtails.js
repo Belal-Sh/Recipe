c7986cea89d1b415f56ab1c7d57c61f5f4d679af
taha148 <64436466+taha148@users.noreply.github.com>
2022-02-08 20:57:35 +0200

    project(recipe):enhancement remove navigate and onclick

--- src/components/CardDtails/CardDtails.js
+++ src/components/CardDtails/CardDtails.js
@@ -1,10 +1,9 @@
 import React from "react";
 import css from "./CardDtails.module.css"
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ImageCopmonet from "../../utils/Image";
 const CardDtails = (props) => {
     const { title, image, id } = props
-    let navigate = useNavigate();
     return (
         <>
             <Link to={`/recipe/${id}`} className={css.cardWrap}>
@@ -13,7 +12,7 @@ const CardDtails = (props) => {
                         <ImageCopmonet imgSrc={image} altImage={title} styles={css.imageContainer} />
                         <div className={css.generItem}>
                             <h2 className={css.recipeTitle}>{title}</h2>
-                            <button className={css.buttonRedirect} onClick={() => navigate(`/recipe/${id}`)}>more details</button>
+                            <button className={css.buttonRedirect}>more details</button>
                         </div>
 
                     </div>
